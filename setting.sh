d8c08aa3e2ec1b13b144e9f279831d654933c676
Seongjun Kim <seongjki@c8r3s2.42seoul.kr>
2023-07-03 18:26:41 +0900

    setting.sh 스크립트 각 디렉토리 따로 검사하고 생성하도록 수정

--- setting.sh
+++ setting.sh
@@ -1,12 +1,34 @@
-#/bin/bash
+#!/bin/bash
 
 SHELL_PROFILE=$HOME/.zshrc
 
-mkdir $HOME/goinfre/.pub-cache
-mkdir $HOME/goinfre/my_gradle
-mkdir $HOME/goinfre/android_sdk
+GOINFRE=$HOME/goinfre
 
-if [ x"${PUB_CACHE}"  == "x" ]; then
+PUB_CACHE_DIR=$GOINFRE/.pub-cache
+GRADLE_DIR=$GOINFRE/my_gradle
+ANDROID_SDK_DIR=$GOINFRE/android_sdk
+
+if [ -d $PUB_CACHE_DIR ]; then
+	echo ".pub_cache directory is already exits"
+else
+	mkdir $PUB_CACHE_DIR
+fi
+
+
+if [ -d $GRADLE_DIR ]; then
+	echo "my_gradle directory is already exits"
+else
+	mkdir $GRADLE_DIR
+fi
+
+if [ -d $ANDROID_SDK_DIR ]; then
+	echo "android_sdk directory is already exits"
+else
+	mkdir $ANDROID_SDK_DIR
+fi
+
+	
+if [ x"${PUB_CACHE}"  == "x" ] && [ grep $HOME/goinfre/.pub-cache $HOME/.zshrc ]; then
    echo "export PUB_CACHE=$HOME/goinfre/.pub-cache" >> $SHELL_PROFILE
    echo "export GRADLE_USER_HOME=$HOME/goinfre/my_gradle" >> $SHELL_PROFILE
    echo "export PATH=$PATH:$HOME/goinfre/flutter/bin" >> $SHELL_PROFILE
