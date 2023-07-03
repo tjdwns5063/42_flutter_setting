cffb70485702b73194907efa44076c8a689b3f53
Seongjun Kim <seongjki@c8r3s2.42seoul.kr>
2023-07-03 18:40:06 +0900

    Fix: setting.sh
    - 각 환경변수마다 설정 됐는지 체크하고 추가하는 부분 분리.

--- setting.sh
+++ setting.sh
@@ -26,12 +26,27 @@ if [ -d $ANDROID_SDK_DIR ]; then
 else
 	mkdir $ANDROID_SDK_DIR
 fi
-
 	
-if [ x"${PUB_CACHE}"  == "x" ] && [ grep $HOME/goinfre/.pub-cache $HOME/.zshrc ]; then
+RET=`grep $GOINFRE/.pub-cacher $SHELL_PROFILE | wc -l`
+
+if [ x"${PUB_CACHE}"  == "x" ] && [ RET == "0" ]; then
    echo "export PUB_CACHE=$HOME/goinfre/.pub-cache" >> $SHELL_PROFILE
-   echo "export GRADLE_USER_HOME=$HOME/goinfre/my_gradle" >> $SHELL_PROFILE
-   echo "export PATH=$PATH:$HOME/goinfre/flutter/bin" >> $SHELL_PROFILE
 else
  echo "env is already set"
+fi
+
+RET=`grep $GOINFRE/my_gradle $SHELL_PROFILE | wc -l`
+
+if [ x"${GRADLE_USER_HOME}" == "x" ]  && [ RET == "0" ]; then	
+	echo "export GRADLE_USER_HOME=$HOME/goinfre/my_gradle" >> $SHELL_PROFILE
+else
+	echo "gradle env is already set"
+fi
+
+RET=`grep $GOINFRE/flutter $SHELL_PROFILE | wc -l`
+
+if [ RET == "0" ]; then	
+   echo "export PATH=$PATH:$HOME/goinfre/flutter/bin" >> $SHELL_PROFILE
+else
+	echo "flutter env is already set"
 fi
