8ce62d86bb0d13e3037f4856a8c901308ce84a06
Seongjun Kim <seongjki@c6r5s7.42seoul.kr>
2023-07-27 14:09:39 +0900

    Fix: fix script

--- setting.sh
+++ setting.sh
@@ -30,9 +30,9 @@ else
 	echo "android_sdk dir is created"
 fi
 	
-RET=`grep $GOINFRE/.pub-cacher $SHELL_PROFILE | wc -l`
+RET=`grep $GOINFRE/.pub-cache $SHELL_PROFILE | wc -l`
 
-if [ x"${PUB_CACHE}"  == "x" ] && [ RET == "0" ]; then
+if [ $RET == 0 ]; then
    echo "export PUB_CACHE=$HOME/goinfre/.pub-cache" >> $SHELL_PROFILE
 else
  echo "env is already set"
@@ -40,7 +40,7 @@ fi
 
 RET=`grep $GOINFRE/my_gradle $SHELL_PROFILE | wc -l`
 
-if [ x"${GRADLE_USER_HOME}" == "x" ]  && [ RET == "0" ]; then	
+if [ $RET == "0" ]; then	
 	echo "export GRADLE_USER_HOME=$HOME/goinfre/my_gradle" >> $SHELL_PROFILE
 else
 	echo "gradle env is already set"
@@ -48,7 +48,7 @@ fi
 
 RET=`grep $GOINFRE/flutter $SHELL_PROFILE | wc -l`
 
-if [ RET == "0" ]; then	
+if [ $RET == "0" ]; then	
    echo "export PATH=$PATH:$HOME/goinfre/flutter/bin" >> $SHELL_PROFILE
 else
 	echo "flutter env is already set"
